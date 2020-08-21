dde6d9b1cb7246a112f61357ebe1b0f0e3beb27a
James Robertson <james@jamesrobertson.org>
2020-08-20 21:58:45 -0400

    update buttons

---
+++ components/MainButton.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import colors from "../constants/Colors";
+
+const MainButton = (props) => {
+  return (
+    <TouchableOpacity activeOpacity={0.6} onPress={props.onPress}>
+      <View style={styles.button}>
+        <Text style={styles.buttonText}>{props.title}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: colors.foreground,
+    paddingVertical: 12,
+    paddingHorizontal: 30,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 18,
+  },
+});
+
+export default MainButton;
--- screens/CombatScreen.js
+++ screens/CombatScreen.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { StyleSheet, Text, View, ScrollView } from "react-native";
+import { StyleSheet, View, ScrollView } from "react-native";
 import Colors from "../constants/Colors";
 import { Card, Button } from "react-native-elements";
 import WeaponCard from "../components/WeaponCard";
+import MainButton from "../components/MainButton";
 
 export default function CombatScreen(props) {
   const uuid = require("uuid");
@@ -18,8 +19,8 @@ export default function CombatScreen(props) {
           containerStyle={styles.healthContainer}
           wrapperStyle={styles.healthContainerWrapper}
         >
-          <Button title={"Wounds: " + props.character.traits.wounds} />
-          <Button title={"Shock: " + props.character.traits.shock} />
+          <MainButton title={"Wounds: " + props.character.traits.wounds} />
+          <MainButton title={"Shock: " + props.character.traits.shock} />
         </Card>
         {weapons.map((w) => (
           <WeaponCard weapon={w.value.w} myKey={w.id} />
@@ -55,4 +56,7 @@ const styles = StyleSheet.create({
     justifyContent: "space-around",
     paddingTop: 10,
   },
+  button: {
+      color: Colors.foreground
+  }
 });
